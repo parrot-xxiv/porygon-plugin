fc700d754bcf612f75a94095650bdd916aea457d
parrot-xxiv <parrot-xxiv@users.noreply.github.com>
2025-04-02 08:53:05 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Porygon Plugin
+### Adding records (CPT) upon the upload of files, with CSV and XLSX formats as acceptable options.
---
+++ porygon.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name:     Porygon Plugin 
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     Adding records (CPT) upon the upload of files, with CSV and XLSX formats as acceptable options.
+ * Author:          Eren 
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     entry
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Porygon
+ */
+
+// Your code starts here.
+
+include plugin_dir_path( __FILE__ ) . 'inc/plugin-page.php';
